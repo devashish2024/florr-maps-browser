11817fa56f9ba46cdc478f29a62db2d8998d97b4
Ashish Agarwal <code.with.aasheesh@gmail.com>
2026-03-08 07:03:31 +0530

    obfuscate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,65 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import obfuscator from "vite-plugin-javascript-obfuscator"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    obfuscator({
+      apply: "build",
+      options: {
+        compact: true,
+
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+
+        debugProtection: true,
+        debugProtectionInterval: 2000,
+
+        disableConsoleOutput: true,
+
+        identifierNamesGenerator: "hexadecimal",
+
+        numbersToExpressions: true,
+
+        renameGlobals: false,
+
+        selfDefending: true,
+
+        simplify: true,
+
+        splitStrings: true,
+        splitStringsChunkLength: 4,
+
+        stringArray: true,
+        stringArrayEncoding: ["base64"],
+        stringArrayThreshold: 0.75,
+
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+
+        transformObjectKeys: true
+      }
+    })
+  ],
+
+  build: {
+    sourcemap: false,
+    minify: "terser",
+
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 3
+      },
+      mangle: {
+        toplevel: true
+      }
+    }
+  }
 })
